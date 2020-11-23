909697328b4b296a96f6bd6d1061ae9631dee36e
Afaque H <afaque.hussain@outlook.com>
2020-11-23 17:34:01 +0200

    Create a construct and use it

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,18 +1,35 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+
+/**
+ * Import a construct that you defined.
+ */
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_10_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler', // file is "hello", function is "handler"
     });
 
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+    /**
+     * Using the custom defined construct.
+     */
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    /**
+     * Creates an API gateway and hooks upto the lambda function.
+     * How does it get it's permission to invoke the lambda function?
+     */
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: helloWithCounter.handler, // where helloWithCounter.handler is a variable of lambda.Function
+    });
   }
 }
---
+++ lib/hitcounter.ts
@@ -0,0 +1,45 @@
+import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
+
+export interface HitCounterProps {
+  downstream: lambda.IFunction;
+}
+
+export class HitCounter extends cdk.Construct {
+  /**
+   * Declared a class variable.
+   */
+  public readonly handler: lambda.Function;
+
+  constructor(scope: cdk.Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    /**
+     * Declared a new dynamodb table.
+     */
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'hitcounter.handler',
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName,
+      },
+    });
+
+    /**
+     * Allow the lambda function to read and write to the table.
+     */
+    table.grantReadWriteData(this.handler);
+
+    /**
+     * Allow the the lambda function to call the downstream lambda function.
+     */
+    props.downstream.grantInvoke(this.handler);
+  }
+}
