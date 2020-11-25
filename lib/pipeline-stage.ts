379a02120907efb876c293adb4ab8be8fa21f3cf
Afaque H <afaque.hussain@outlook.com>
2020-11-25 10:52:44 +0200

    Trying to trigger a build

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -7,7 +7,6 @@ import { Stage, Construct, StageProps } from '@aws-cdk/core';
 export class WorkshopPipelineStage extends Stage {
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
-
     new CdkWorkshopStack(this, 'WebService');
   }
 }
