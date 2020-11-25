a7035a0975a72e5ffd7326f1950368fbb93c2621
Afaque H <afaque.hussain@outlook.com>
2020-11-25 15:22:45 +0200

    Add a GraphQl API

--- lib/ci-cd-pipeline/pipeline-stage.ts
+++ lib/ci-cd-pipeline/pipeline-stage.ts
@@ -1,4 +1,5 @@
 import { HitCounterStack } from '../hit-counter-stack/hit-counter-stack';
+import { GraphQlStack } from '../graphql-stack/graphql-stack';
 import { Stage, CfnOutput, Construct, StageProps } from '@aws-cdk/core';
 
 /**
@@ -7,9 +8,15 @@ import { Stage, CfnOutput, Construct, StageProps } from '@aws-cdk/core';
 export class WorkshopPipelineStage extends Stage {
   public readonly hcViewerUrl: CfnOutput;
   public readonly hcEndpoint: CfnOutput;
+  public readonly graphQlEndpoint: CfnOutput;
+  public readonly graphQlApiKey: CfnOutput;
 
   constructor(scope: Construct, id: string, props?: StageProps) {
     super(scope, id, props);
+
+    const graphQlApi = new GraphQlStack(this, 'GraphQlApi');
+    this.graphQlEndpoint = graphQlApi.graphQlEndpoint;
+    this.graphQlApiKey = graphQlApi.graphQlApiKey;
 
     const service = new HitCounterStack(this, 'WebService');
     this.hcEndpoint = service.hcEndpoint;
---
+++ lib/graphql-stack/graphql-stack.ts
@@ -0,0 +1,34 @@
+import { CdkPipeline } from '@aws-cdk/pipelines';
+import * as cdk from '@aws-cdk/core';
+import * as appsync from '@aws-cdk/aws-appsync';
+
+export class GraphQlStack extends cdk.Stack {
+  public readonly graphQlEndpoint: cdk.CfnOutput;
+  public readonly graphQlApiKey: cdk.CfnOutput;
+
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const graphQlApi = new appsync.GraphqlApi(this, 'MainApi', {
+      name: 'main-graphql-api',
+      schema: appsync.Schema.fromAsset(`${__dirname}/schema.graphql`),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          // apiKeyConfig: {
+          //   expires: cdk.Expiration.atTimestamp(1631579248000),
+          // },
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    this.graphQlEndpoint = new cdk.CfnOutput(this, 'GraphQlApiUrl', {
+      value: graphQlApi.graphqlUrl,
+    });
+
+    this.graphQlApiKey = new cdk.CfnOutput(this, 'GraphQlApiKey', {
+      value: graphQlApi.apiKey || '',
+    });
+  }
+}
